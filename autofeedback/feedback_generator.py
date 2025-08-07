0c4c0e0e180e457da006cf97ffc8d28401ed9bad
kaierih <kai.e.hoff@ntnu.no>
2025-08-07 10:58:46 +0200

    Updated feedback generator to allow selecting specific problems to grade

--- autofeedback/feedback_generator.py
+++ autofeedback/feedback_generator.py
@@ -100,6 +100,20 @@ def autograde_notebooks(notebook_list):
     nbgrader, with the addition of hidden tests being copied to metadata
     for each grade cell.
     """
+    student_input = input("Enter problem number to grade, or type 'all' to grade the entire assignment.")
+
+    if student_input.isnumeric() and 1 <= int(student_input) <= len(notebook_list) + 1:
+
+
+        notebooks_to_grade = notebook_list[int(student_input) - 1]
+        print(f"Grading problem {notebooks_to_grade[0]}.")
+    elif student_input.lower().strip() == 'all':
+        notebooks_to_grade = notebook_list
+        pring(f"Grading all problems in assignment.")
+    else:
+        print(f"No valid input detected. Nothing to do.")
+
+
     total_score = 0
     max_score = 0
 
@@ -120,7 +134,7 @@ def autograde_notebooks(notebook_list):
         total_score += notebook_score
         max_score += notebook_max
     display(Markdown(
-        """Finished grading all tasks! Final score: %s/%s.""" %
+        """Finished grading task(s)! Final score: %s/%s.""" %
         (str(total_score),
          str(max_score))
     ))
